b7217b166e9c2b69179357d384d2f91f223c994d
Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-08-20 21:12:27 -0300

    feat: add zod schema to parse envs

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+import z from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
+});
+
+const { success, error, data: env } = envSchema.safeParse(process.env);
+
+if (!success) {
+  console.error("Invalid environment variables", z.prettifyError(error));
+
+  throw new Error("Invalid environment variables");
+}
+
+export { env };
