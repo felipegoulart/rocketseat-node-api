055755f5fd72fe4d768aa79938cbe2105a98d23a
Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-08-20 23:08:09 -0300

    feat(transactions): add post transactions route
    This route is used to add transactions with prisma ORM

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,34 @@
-import type { FastifyInstance, FastifyPluginAsync } from "fastify";
+import crypto from "node:crypto";
+import type {
+  FastifyInstance,
+  FastifyPluginAsync,
+  FastifyReply,
+  FastifyRequest,
+} from "fastify";
+import z from "zod";
+import { prisma } from "../infra/database/prisma";
 
 export const transactionRoutes: FastifyPluginAsync = async (
   app: FastifyInstance,
 ) => {
-  app.get("/hello", () => {
-    return "Hello World";
+  app.post("/", async (request: FastifyRequest, reply: FastifyReply) => {
+    const bodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const { title, amount, type } = bodySchema.parse(request.body);
+
+    await prisma.transaction.create({
+      data: {
+        amount,
+        title,
+        type,
+        sessionId: crypto.randomUUID(),
+      },
+    });
+
+    return reply.status(201).send();
   });
 };
