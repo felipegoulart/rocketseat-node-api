ae068cecf8135194ac84ef748e29605cb033565c
Felipe Goulart <luisfelipegoulart94@gmail.com>
2025-08-21 15:28:19 -0300

    feat(zod): configure zod validator

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,34 +1,38 @@
 import crypto from "node:crypto";
+import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import type {
-  FastifyInstance,
-  FastifyPluginAsync,
-  FastifyReply,
-  FastifyRequest,
-} from "fastify";
+  FastifyPluginAsyncZod,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
 import z from "zod";
 import { prisma } from "../infra/database/prisma";
 
-export const transactionRoutes: FastifyPluginAsync = async (
+export const transactionRoutes: FastifyPluginAsyncZod = async (
   app: FastifyInstance,
 ) => {
-  app.post("/", async (request: FastifyRequest, reply: FastifyReply) => {
-    const bodySchema = z.object({
-      title: z.string(),
-      amount: z.number(),
-      type: z.enum(["credit", "debit"]),
-    });
+  app.withTypeProvider<ZodTypeProvider>().route({
+    method: "POST",
+    url: "/",
+    schema: {
+      body: z.object({
+        title: z.string(),
+        amount: z.number(),
+        type: z.enum(["credit", "debit"]),
+      }),
+    },
+    handler: async (request, reply) => {
+      const { title, amount, type } = request.body;
 
-    const { title, amount, type } = bodySchema.parse(request.body);
+      await prisma.transaction.create({
+        data: {
+          amount,
+          title,
+          type,
+          sessionId: crypto.randomUUID(),
+        },
+      });
 
-    await prisma.transaction.create({
-      data: {
-        amount,
-        title,
-        type,
-        sessionId: crypto.randomUUID(),
-      },
-    });
-
-    return reply.status(201).send();
+      return reply.status(201).send();
+    },
   });
 };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,48 @@
 import fastify from "fastify";
+import {
+  hasZodFastifySchemaValidationErrors,
+  isResponseSerializationError,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
 import { env } from "./env";
 import { transactionRoutes } from "./routes/transactions";
 
 const app = fastify();
 
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
 app.register(transactionRoutes, {
   prefix: "transactions",
+});
+
+app.setErrorHandler((error, req, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.code(400).send({
+      error: "Response Validation Error",
+      message: "Request doesn't match the schema",
+      statusCode: 400,
+      details: {
+        issues: error.validation,
+        method: req.method,
+        url: req.url,
+      },
+    });
+  }
+
+  if (isResponseSerializationError(error)) {
+    return reply.code(500).send({
+      error: "Internal Server Error",
+      message: "Response doesn't match the schema",
+      statusCode: 500,
+      details: {
+        issues: error.cause.issues,
+        method: error.method,
+        url: error.url,
+      },
+    });
+  }
 });
 
 app.listen({ port: env?.PORT }).then(() => {
